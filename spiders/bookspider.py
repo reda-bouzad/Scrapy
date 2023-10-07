e80060b8c488d01e2480e0f898c29334d69271d8
reda-bouzad <redabouzadfst@gmail.com>
2023-10-07 20:23:51 +0100

    v2

--- spiders/bookspider.py
+++ spiders/bookspider.py
@@ -14,3 +14,13 @@ class BookspiderSpider(scrapy.Spider):
                 'price': book.css('.price_color::text').get(),
                 'url': book.css('h3 a').attrib['href']
             }
+        
+        nextPage = response.css('li.next a').attrib['href']
+
+        if nextPage is not None:
+            if 'catalogue/' in nextPage:
+                nextPageUrl = "https://books.toscrape.com/"+ nextPage
+                yield response.follow(nextPageUrl, callback= self.parse)
+            else:
+                nextPageUrl = "https://books.toscrape.com/catalogue/" + nextPage
+                yield response.follow(nextPageUrl, callback= self.parse)
